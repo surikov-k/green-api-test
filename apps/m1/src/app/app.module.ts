e7b2c5c4e59abb4296d8ec3d4c2edc0a900c720d
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2023-07-25 15:54:43 +0300

    [m2] Add configuration

--- apps/m1/src/app/app.module.ts
+++ apps/m1/src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { SomeModuleModule } from './some-module/some-module.module';
 
 @Module({
   imports: [SomeModuleModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- apps/m2/src/app/app.module.ts
+++ apps/m2/src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { SomeModuleModule } from './some-module/some-module.module';
+import { ENV_FILE_PATH } from './app.constants';
+import databaseConfig from '../config/database.config';
+import { validateEnvironment } from './env.validation';
 
 @Module({
-  imports: [SomeModuleModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      envFilePath: ENV_FILE_PATH,
+      load: [databaseConfig],
+      validate: validateEnvironment,
+    }),
+    SomeModuleModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
